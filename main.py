82d14f76c3999f6ae695d06a15155c444e232669
cutty853 <cutty853@gmail.com>
2018-03-01 19:24:57 +0100

    Adding a basic main

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# coding: utf-8
+
+"""
+  PySMAnalyzer is a source-monitor report analyzer. It will analyze an XML
+  source-monitor checkpoint and say what's wrong according to rules created
+  by the user.
+"""
+
+
+def main():
+    """ main du programme """
+    print("PySMAnalyzer programmed with love by cutty853")
+
+
+if __name__ == "__main__":
+    main()
