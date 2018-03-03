cad2644d153a3edbb3e6e06f6f325793b3d467e0
cutty853 <cutty853@gmail.com>
2018-03-03 01:56:56 +0100

    Doing the setter for the FunctionMetrics and FileMetrics classes

--- entity/metrics.py
+++ entity/metrics.py
@@ -7,6 +7,8 @@
 
 
 class FileMetrics:
+    """ The class represent the metrics of a file """
+
     def __init__(self, metrics):
         self._metrics = metrics.copy()
 
@@ -19,8 +21,14 @@ class FileMetrics:
         """ Return the value of the metrics named id_name """
         return self._metrics[id_name]
 
+    def set(self, id_name, new_value):
+        """ setter for the metrics named id_name """
+        self._metrics[id_name] = new_value
+
 
 class FunctionMetrics:
+    """ The class represent the metrics of a function """
+
     def __init__(self, complexity=0, statements=0, maximum_depth=0, calls=0):
         self.complexity = complexity
         self.statements = statements
@@ -33,3 +41,19 @@ class FunctionMetrics:
         return f_string.format(
             self.complexity, self.statements, self.maximum_depth, self.calls
         )
+
+    def set_complexity(self, complexity):
+        """ complexity setter """
+        self.complexity = complexity
+
+    def set_statements(self, statements):
+        """ statements setter """
+        self.statements = statements
+
+    def set_maximum_depth(self, maximum_depth):
+        """ maximum_depth setter """
+        self.maximum_depth = maximum_depth
+
+    def set_calls(self, calls):
+        """ calls setter """
+        self.calls = calls
