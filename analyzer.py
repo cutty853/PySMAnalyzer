bc6550eba7bda08435efda0f77520a0de636b941
cutty853 <cutty853@gmail.com>
2018-03-17 14:49:33 +0100

    improve printing of bad files and functions

--- analyzer.py
+++ analyzer.py
@@ -7,9 +7,12 @@
 """
 
 from lxml import etree
-from utils import colourizer
+from utils import colourizer, center
 import reader.smreader as smreader
 import entity.files as files
+
+
+TITLE_WIDTH = 40
 
 
 class Analyzer:
@@ -40,20 +43,39 @@ class Analyzer:
             # Finally add it to the files set
             self.files.add(add_file)
 
-    def print_bad_entities(self):  # pragma: no cover
-        """ Print all bad function with colors """
+    def print_bad_files(self):  # pragma: no cover
+        """ print all the bad files """
         for file_ in self.files:
             if not file_.validity:
                 print("File {} has bad metrics".format(
                     colourizer.color_file(file_.name)
                 ))
-            else:
+
+    def print_bad_functions(self):  # pragma: no cover
+        """ print all the bad functions of each file """
+        for file_ in self.files:
+            if file_.has_bad_functions():
                 print("File", colourizer.color_file(file_.name))
-            for function in file_.functions:
-                if not function.validity:
-                    print("{} Function {} from {} has bad metrics".format(
-                        colourizer.error("■"),
-                        colourizer.color_function(function.name),
-                        file_.name
-                    ))
-            print()
+
+                for function in file_.functions:
+                    if not function.validity:
+                        print("{} Function {} from {} has bad metrics".format(
+                            colourizer.error("■"),
+                            colourizer.color_function(function.name),
+                            file_.name
+                        ))
+
+                print()
+
+    def print_bad_entities(self):  # pragma: no cover
+        """ Print all bad function with colors """
+        print("-" * TITLE_WIDTH)
+        print(center("Bad files", TITLE_WIDTH))
+        print("-" * TITLE_WIDTH)
+        self.print_bad_files()
+        print("\n\n", end='')
+
+        print("-" * TITLE_WIDTH)
+        print(center("Bad functions", TITLE_WIDTH))
+        print("-" * TITLE_WIDTH)
+        self.print_bad_functions()
